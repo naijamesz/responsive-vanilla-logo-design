56e0fb2db002b4b3c3506496ba804c714aba8ad6
naijamesz <kodlikejamesz@hotmail.com>
2023-12-05 18:29:48 +0700

    Initial commit.

---
+++ Content.js
@@ -0,0 +1,33 @@
+export const content = `<div class="container">
+    <div class="col"><img src="images/image1@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing <span class="highlight">elit</span>.</p>
+    </div>
+    <div class="col"><img src="images/image2@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col"><img src="images/image3@2x.png" alt="">
+        <p>Branding: I'm going to try an impress you with my typing skills.</p>
+    </div>
+    <div class="col"><img src="images/image4@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col hide-mobile"><img src="images/image5@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col hide-mobile"><img src="images/image6@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col hide-mobile"><img src="images/image7@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col hide-mobile"><img src="images/image8@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col hide-mobile"><img src="images/image9@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col hide-desktop-only"><img src="images/image10@2x.png" alt="">
+        <p>Branding: Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+
+</div>`;
---
+++ main.js
@@ -0,0 +1,40 @@
+import './style.css';
+// import './css-reset.css';
+import { content } from './Content';
+
+document.querySelector('#app').innerHTML = `
+        <div class="header-container">
+            <header>
+                <div class="logo"><span class="highlight">Responsive Workshop</span> - LOGO DESIGN</div>
+                <nav>
+                    <a href="#" class="mybutton">ABOUT</a>
+                    <a href="#" class="mybutton">PRICING</a>
+                    <a href="#" class="mybutton">CONTACT</a>
+                </nav>
+
+                <div class="mob-wrapper">
+
+                    <img src="images/icon-burger-menu.svg" alt="Mobile Menu Icon" style="width: 40px;" class="burgermenu">
+
+                    <div class="mob-nav">
+                        <a href="#" class="navbutton">ABOUT</a>
+                        <a href="#" class="navbutton">PRICING</a>
+                        <a href="#" class="navbutton">CONTACT</a>
+                    </div>
+
+
+                </div>
+
+            </header>
+        </div>
+        ${content}
+`;
+const burgerMenu = document.querySelector('.burgermenu');
+const mobNav = document.querySelector('.mob-nav');
+
+document.ready(function () {
+  burgerMenu.addEventListener('click', e => {
+    mobNav.toggle();
+  });
+});
+// setupCounter(document.querySelector('#counter'));
